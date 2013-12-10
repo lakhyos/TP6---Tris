e10ba18e67571c33aa1d78fee15cb830b69c9cf1
Med Ayoub <lakhyos@MacBook-de-Med-Ayoub.local>
2013-12-10 20:17:05 +0000

    Initial Commit

---
+++ TP6 - Tris/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TP6 - Tris
+//
+//  Created by Med Ayoub on 10/12/2013.
+//  Copyright (c) 2013 Med Ayoub. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
